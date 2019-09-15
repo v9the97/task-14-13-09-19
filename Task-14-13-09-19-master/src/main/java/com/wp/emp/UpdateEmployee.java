82ce6ba1ca5bb1a9e59fcbf98048efc339f96938
Vinod <v9the97@gmail.com>
2019-09-16 04:37:11 +0530

    tas-14 completed

---
+++ Task-14-13-09-19-master/src/main/java/com/wp/emp/UpdateEmployee.java
@@ -0,0 +1,33 @@
+package com.wp.emp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateEmployee {
+
+	public static void main(String[] args) {
+			
+
+		Employee e1=new Employee(100,"Manav",5000);
+		
+
+		Configuration config=new Configuration().configure();
+		SessionFactory sf=config.buildSessionFactory();
+		Session session=sf.openSession();
+		Transaction tr=session.beginTransaction();
+
+		session.update(e1);
+		
+		tr.commit();
+		
+		session.close();
+		
+		System.out.println("Data Updated");
+		
+		
+		
+	}
+
+}
